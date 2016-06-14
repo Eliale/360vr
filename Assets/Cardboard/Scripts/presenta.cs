761f1a02bd00f4aff291baf1fb2ad75b85a97d18
codeneomatrix <josuecevedo@gmail.com>
2016-06-14 06:45:09 -0500

    agregando tutorial

---
+++ Assets/Cardboard/Scripts/presenta.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class presenta : MonoBehaviour {
+
+
+
+    public GUIStyle estilo;
+
+    void OnGUI()
+    {
+        
+
+        if (GUI.Button(new Rect((Screen.width / 2) * 0.4f, (Screen.height / 2) * 0.95f, (Screen.width) * 0.60f, (Screen.height) * 0.20f), "Tutorial", estilo))
+        {
+            SceneManager.LoadScene("tuto");
+        }
+
+        if (GUI.Button(new Rect((Screen.width / 2) * 0.4f, (Screen.height) * 0.75f, (Screen.width) * 0.60f, (Screen.height) * 0.20f), "Ingresar", estilo))
+        {
+            SceneManager.LoadScene("menu");
+        }
+    }
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Cardboard/Scripts/stuto.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class stuto : MonoBehaviour {
+    public int numerocubo;
+    private float ini;
+    private bool fun;
+    private float tiempo = variables.tiempo;
+    public void SetGazedAt(bool gazedAt)
+    {
+
+        GetComponent<Renderer>().material.color = Color.green;
+        ini = Time.time;
+        fun = true;
+        //GameObject.Find("letrero").GetComponent<TextMesh>().text = "   " + "Evaluando!!!!";
+    }
+
+    public void OnGazeEnter()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnGazeExit()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        GameObject.Find("p").GetComponent<TextMesh>().text = "";
+        GameObject.Find("r3").GetComponent<TextMesh>().text = "";
+        GameObject.Find("r2").GetComponent<TextMesh>().text = "";
+
+        fun = false;
+    }
+
+    public void OnGazeTrigger()
+    {
+        throw new NotImplementedException();
+    }
+    void Update()
+    {
+
+        if (fun)
+        {
+         
+            if ((Time.time - ini) >= tiempo)
+            {
+                //GameObject.Find("letrero").GetComponent<TextMesh>().text = "CARGADO!!!!";
+                fun = false;
+
+                if (numerocubo == 1)
+                {
+                    GameObject.Find("r2").GetComponent<TextMesh>().text = "ha elegido la esfera";
+                    GameObject.Find("r3").GetComponent<TextMesh>().text = "";
+                    GameObject.Find("p").GetComponent<TextMesh>().text = "";
+                }
+                if (numerocubo == 2)
+                {
+                    GameObject.Find("r3").GetComponent<TextMesh>().text = "ha elegido el cubo";
+                    GameObject.Find("r2").GetComponent<TextMesh>().text = "";
+                    GameObject.Find("p").GetComponent<TextMesh>().text = "";
+                }
+                if (numerocubo == 3)
+                {
+                    GameObject.Find("p").GetComponent<TextMesh>().text = "ha elegido el cilindro";
+                    GameObject.Find("r3").GetComponent<TextMesh>().text = "";
+                    GameObject.Find("r2").GetComponent<TextMesh>().text = "";
+                }
+
+            }
+        }
+    }
+}
